cec391aa09c73a4e76a53e843e97979e86571626
Rubel Hosen <rhosen@epharmasolutions.com>
2021-09-15 23:32:57 +0600

    rephrased another comment

--- UploadFileToAzureBlobStorage/Program.cs
+++ UploadFileToAzureBlobStorage/Program.cs
@@ -10,7 +10,7 @@ namespace UploadFileToAzureBlobStorage
             //var files = FileHelper.Files;
             //Prints all the file names you have in the directory
             //FileHelper.PrintFileNames(files);
-            //TODO: Check Validation for multiple files
+            //TODO: Check validation for multiple files
             //if (!files.Any()) Console.WriteLine("Nothing to process");
             var file = FileHelper.GetFile("C:\\BlobTest\\Late Night Poetry 2.txt");
             if (file == null) Console.WriteLine("Nothing to process");
