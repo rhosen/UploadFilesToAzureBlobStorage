716a2ce64137b022bdc77c10465f7e25d6dd51fd
Rubel Hosen <rhosen@epharmasolutions.com>
2021-09-15 23:32:04 +0600

    rephrased some comments

--- UploadFileToAzureBlobStorage/Program.cs
+++ UploadFileToAzureBlobStorage/Program.cs
@@ -5,19 +5,19 @@ namespace UploadFileToAzureBlobStorage
     {
         static void Main(string[] args)
         {
-            // TODO: Don't forget to configure the appSetting.json file
-            // TODO: Use this one if you want to upload multiple files at once
+            //TODO: Don't forget to configure the appSetting.json file
+            //TODO: Use this one if you want to upload multiple files at once
             //var files = FileHelper.Files;
-            // TODO: Prints all the file names you have in the directory
+            //Prints all the file names you have in the directory
             //FileHelper.PrintFileNames(files);
-            // TODO: Validation for multiple files
+            //TODO: Check Validation for multiple files
             //if (!files.Any()) Console.WriteLine("Nothing to process");
             var file = FileHelper.GetFile("C:\\BlobTest\\Late Night Poetry 2.txt");
             if (file == null) Console.WriteLine("Nothing to process");
             else
             {
                 var blobHelper = new BlobHelper(FileHelper.ConnectionString, FileHelper.ContainerName);
-                // TODO: for multiple files
+                // TODO: Upload multiple files
                 //blobHelper.UploadFiles(FileHelper.Files, FileHelper.ConnectionString, FileHelper.ContainerName);
                 Console.WriteLine("Uploading...");
                 blobHelper.UploadFile(file, false);
